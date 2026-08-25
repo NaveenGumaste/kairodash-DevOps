dad5f6e404ebdc6b406c705ca51bb100363f1808
naveengumaste <ngumaste03@gmail.com>
2026-08-25 16:02:28 +0530

    fix(ci): specify OIDC audience and broaden IAM trust policy sub pattern

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -43,11 +43,12 @@ data "aws_iam_policy_document" "github_actions_trust" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:NaveenGumaste@64587607/kairodash-DevOps@1331818586:ref:refs/heads/devops/initial-setup"
+        "repo:NaveenGumaste/kairodash-DevOps:*",
+        "repo:NaveenGumaste@64587607/kairodash-DevOps@1331818586:*"
       ]
     }
   }
